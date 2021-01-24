8ff3682ec0254333e742f91d08e8a38f057e0e73
giden.dev <support@giden.co>
2021-01-23 20:14:49 -0500

    add summary + run prettier

--- pest/classes.py
+++ pest/classes.py
@@ -2,11 +2,13 @@ import multiprocessing
 import traceback
 from colorterminal import ColorText
 
+
 class Test:
     def __init__(self, name, func):
         self.name = name
         self.func = func
-    
+        self.successful = False
+
     @classmethod
     def test(cls, name: str, func):
         return cls(name, func)
@@ -15,50 +17,85 @@ class Test:
     def it(cls, name: str, func):
         return cls(name, func)
 
+
+class Summary:
+    def __init__(self, suites: list):
+        self.suites = suites
+
+    def suites_passed(self) -> str:
+        return f"{len([suite for suite in self.suites if suite.successful])} of {len(self.suites)} suites passed"
+
+    def tests_passed(self) -> str:
+        total_tests = sum([len(suite.tests) for suite in self.suites])
+        passed = 0
+        for suite in self.suites:
+            passed += len([t for t in suite.tests_successful if t])
+
+        return f"{passed} of {total_tests} tests passed"
+
+
 class TestSuite:
     def __init__(self, suite_name: str, tests: list):
-      self.name = suite_name
-      self.tests = tests
-      
+        self.name = suite_name
+        self.tests = tests
+        self.successful = False
+        self.tests_successful = [False for test in tests]
+        self.results = []
+        self.summary = None
+
     @classmethod
     def describe(cls, suite_name: str, tests: list):
         return cls(suite_name, tests)
-    
-    def test_runner(self, func):
+
+    def test_runner(self, test: Test):
         try:
-            func()
+            test.func()
+            test.successful = True
             return True, ""
         except Exception as e:
             exceptiondata = traceback.format_exc().splitlines()
-            return False, exceptiondata[-1] # only print last line of error out
-    
+            return False, exceptiondata[-1]  # only print last line of error out
+
     def find_name(self, i):
         return self.tests[i].name
-    
+
     def run(self, sync=True):
-        print(ColorText.BLUE + "* Running " + self.name.capitalize() + " *")
+
+        print(ColorText.BLUE + "*" + self.name.capitalize() + " *")
+
         pool = multiprocessing.Pool()
-        if not sync: 
-            results = pool.map(self.test_runner, [test.func for test in self.tests])
+        if not sync:
+            self.results = pool.map(self.test_runner, [test for test in self.tests])
         else:
-            results = [self.test_runner(test.func) for test in self.tests]
-        
-        for i,result in enumerate(results):
+            self.results = [self.test_runner(test) for test in self.tests]
+
+        # Eval results
+
+        self.tests_successful = [r[0] for r in self.results]
+        self.successful = len([t for t in self.tests_successful if t]) == len(
+            self.tests
+        )
+
+        # Print results
+
+        for i, result in enumerate(self.results):
             passed = result[0]
             if passed:
-                print(ColorText.GREEN + "✓ " + ColorText.WHITE + " " + self.find_name(i))
+                print(
+                    ColorText.GREEN + "✓ " + ColorText.WHITE + " " + self.find_name(i)
+                )
             else:
                 print(ColorText.RED + "x " + ColorText.WHITE + " " + self.find_name(i))
                 print(result[1])
 
-    
+
 # if __name__ == '__main__':
 #     def good_sum():
 #         assert 1+1 == 2
-    
+
 #     def bad_sum():
 #         assert 1+1 == 1
-    
+
 #     TestSuite.describe("you-cam", [
 #         Test.it("should add two numbers together", good_sum),
 #         Test.it("should not add two numbers together", bad_sum)
--- pest/runner.py
+++ pest/runner.py
@@ -1,20 +1,42 @@
-import glob 
-import os 
+import glob
+import os
+from typing import List
+from .classes import TestSuite, Summary, ColorText
+from thesmuggler import smuggle
 
-def test_suites(test_file: str):
+
+def run_test_suites(suites: List[TestSuite], sync=True):
+
+    print(ColorText.YELLOW + f"Running {len(suites)} test suites \n")
+
+    for suite in suites:
+        suite.run(sync=sync)
+
+    summary = Summary(suites)
+
+    print(ColorText.YELLOW + "\n*** Summary ***")
+    print(ColorText.WHITE + summary.suites_passed())
+    print(ColorText.WHITE + summary.tests_passed())
+
+
+def find_test_suites(test_file: str):
     assert os.path.isfile(test_file)
-    
-    
+    test_module = smuggle(test_file)
+    return [
+        variable
+        for _, variable in test_module.__dict__.items()
+        if isinstance(variable, TestSuite)
+    ]
 
-def find_test_files(dir: str) -> list:
-    '''
+
+def find_test_files(dir: str) -> List[str]:
+    """
     Find test files that match the following naming patter:
     - *test.py
     - test*.py
-    
+
     Search is recursive, so pass in only the parent directory for search
-    '''
+    """
     assert os.path.isdir(dir)
     # abs_dir = os.path.abspath(dir)
     return glob.glob(dir + "**/*test*.py", recursive=True)
-    
